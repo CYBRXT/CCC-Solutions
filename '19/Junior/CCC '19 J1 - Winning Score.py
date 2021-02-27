910934aea080b67da077eacd99f2f5061e2ef610
Geoffrey Kong <79559478+Geoffrey-Kong@users.noreply.github.com>
2021-02-27 00:34:46 -0500

    Create CCC '19 J1 - Winning Score.py

---
+++ '19/Junior/CCC '19 J1 - Winning Score.py
@@ -0,0 +1,18 @@
+a3 = int(input())
+a2 = int(input())
+a1 = int(input())
+b3 = int(input())
+b2 = int(input())
+b1 = int(input())
+
+A = (a3 * 3) + (a2 * 2) + (a1)
+B = (b3 * 3) + (b2 * 2) + (b1)
+
+if A > B:
+  print("A")
+  
+elif A < B:
+  print("B")
+  
+elif A == B:
+  print("T")
