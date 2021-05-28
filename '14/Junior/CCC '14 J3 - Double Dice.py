422660ea6a2a2be52329b5b04158f188b9ddd878
Geoffrey Kong <79559478+CYBRXT@users.noreply.github.com>
2021-05-28 16:08:43 -0400

    Update CCC '14 J3 - Double Dice.py

--- '14/Junior/CCC '14 J3 - Double Dice.py
+++ '14/Junior/CCC '14 J3 - Double Dice.py
@@ -10,10 +10,10 @@ for i in range(numberofrounds):
     d_roll = rolls[1]
 
     if a_roll < d_roll:
-        ascoretotal = ascoretotal - d_roll
+        ascoretotal -= d_roll
         dscoretotal = dscoretotal
     elif a_roll > d_roll:
-        dscoretotal = dscoretotal - a_roll
+        dscoretotal -= a_roll
         ascoretotal = ascoretotal
     else:
         pass
