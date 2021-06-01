6edc63a74eb83b81891e28c71e986d611d8df75f
Geoffrey Kong <79559478+CYBRXT@users.noreply.github.com>
2021-06-01 15:11:30 -0400

    Update CCC '05 J2 - RSA Numbers.py

--- '05/Junior/CCC '05 J2 - RSA Numbers.py
+++ '05/Junior/CCC '05 J2 - RSA Numbers.py
@@ -3,14 +3,12 @@ max = int(input())
 
 RSA = 0
 
-counter = 0
-
-for x in range(min, max):
- for y in range(1, max):
-    if x % y == 0:
+for i in range(min, max + 1):
+  counter = 0
+  for j in range(1, max + 1):
+    if i % j == 0:
       counter += 1
-      
-if counter == 4:
-  RSA += 1
-  
+  if counter == 4:
+    RSA += 1
+
 print(f"The number of RSA numbers between {min} and {max} is {RSA}")
